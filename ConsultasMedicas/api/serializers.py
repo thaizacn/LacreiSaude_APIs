b98cfbd71b59dbb123ef3e856e37b8968af9131c
Thaizinha <thaizacarvalh@gmail.com>
2023-10-28 00:14:49 -0300

    Testes unitários + validações de segurança

--- ConsultasMedicas/api/serializers.py
+++ ConsultasMedicas/api/serializers.py
@@ -5,10 +5,37 @@ class PessoaProfissionalSerializer(serializers.ModelSerializer):
     #Definindo metadados para o serializador
     class Meta:
         model = PessoaProfissional
-        fields = '__all__'
+        fields = ('id', 'nome', 'nome_social')
+        extra_kwargs = {
+            'nome': {'max_length': 100},
+            'nome_social': {'max_length': 100, 'required': False}
+        }
+
+    def validate_nome(self, value):
+        # Verifica se o nome contém apenas letras e espaços
+        if not value.replace(" ", "").isalpha():
+            raise serializers.ValidationError("Nome deve conter apenas letras e espaços.")
+        return value
+
+    def validate_nome_social(self, value):
+        # Verifica se o nome social contém apenas letras e espaços
+        if value and not value.replace(" ", "").isalpha():
+            raise serializers.ValidationError("Nome social deve conter apenas letras e espaços.")
+        return value
 
 class ConsultaSerializer(serializers.ModelSerializer):
-    class Meta:
         #Definindo metadados para o serializador
+    class Meta:
         model = Consulta
-        fields = '__all__'
+        fields = ('id', 'data', 'profissional')
+        extra_kwargs = {
+            'data': {'required': True},
+            'profissional': {'required': True}
+        }
+
+    def validate_data(self, value):
+        # Verifica se a data está no futuro (não permite agendamentos no passado)
+        from datetime import date
+        if value < date.today():
+            raise serializers.ValidationError("A data da consulta deve ser no futuro.")
+        return value
--- ConsultasMedicas/api/tests.py
+++ ConsultasMedicas/api/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import PessoaProfissional, Consulta
+from .serializers import PessoaProfissionalSerializer, ConsultaSerializer
+from datetime import date, timedelta
 
-# Create your tests here.
+class PessoaProfissionalTests(APITestCase):
+    def test_criar_pessoa_profissional(self):
+        data = {'nome': 'Thaiza', 'nome_social': 'Thaiza Social'}
+        response = self.client.post('/api/profissionais/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_listar_pessoa_profissional(self):
+        PessoaProfissional.objects.create(nome='Thaiza', nome_social='Thaiza Social')
+        response = self.client.get('/api/profissionais/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_nome_contem_apenas_letras_e_espacos(self):
+        serializer = PessoaProfissionalSerializer(data={'nome': 'Alice123', 'nome_social': 'Alice Social'})
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('nome', serializer.errors)
+
+    def test_nome_social_contem_apenas_letras_e_espacos(self):
+        serializer = PessoaProfissionalSerializer(data={'nome': 'Alice', 'nome_social': 'Alice123'})
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('nome_social', serializer.errors)
+
+class ConsultaTests(APITestCase):
+    def test_criar_consulta(self):
+        profissional = PessoaProfissional.objects.create(nome='Thaiza', nome_social='Thaiza Social')
+        data = {'data': '2023-11-01', 'profissional': profissional.id}
+        response = self.client.post('/api/consultas/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_listar_consulta(self):
+        profissional = PessoaProfissional.objects.create(nome='Thaiza', nome_social='Thaiza Social')
+        Consulta.objects.create(data='2023-11-01', profissional=profissional)
+        response = self.client.get('/api/consultas/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+#    def test_data_no_futuro(self):
+#        data_passada = date.today() - timedelta(days=1)  # Data passada
+#        serializer = ConsultaSerializer(data={'data': data_passada, 'profissional': self.profissional.id})
+#        self.assertFalse(serializer.is_valid())
+#        self.assertIn('data', serializer.errors)
